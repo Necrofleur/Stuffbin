bc794ec745b7a41aab9f9927345c00e88b58dc03
Inken Kotulla <inken.kotulla@tsbw.cloud>
2020-02-20 13:04:54 +0100

    Inital Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MeineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 5;
+            Console.WriteLine(AddiereZahlen(a, b).ToString());
+            Console.ReadLine();
+        }
+
+        public static int AddiereZahlen (int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
